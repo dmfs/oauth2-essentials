21f5bd62f4a918e77f2005d83d3bf8e9aebd29b5
Marten Gajda <marten@dmfs.org>
2019-09-18 17:26:08 +0200

    Fix previous attempt of supporting no rdirect URL, implements #71

--- src/main/java/org/dmfs/oauth2/client/BasicOAuth2Client.java
+++ src/main/java/org/dmfs/oauth2/client/BasicOAuth2Client.java
@@ -25,7 +25,6 @@ import org.dmfs.httpessentials.exceptions.UnexpectedStatusException;
 import org.dmfs.httpessentials.executors.useragent.Branded;
 import org.dmfs.httpessentials.types.Product;
 import org.dmfs.httpessentials.types.VersionedProduct;
-import org.dmfs.oauth2.client.utils.LazyCharSequence;
 import org.dmfs.rfc3986.Uri;
 import org.dmfs.rfc3986.encoding.Precoded;
 import org.dmfs.rfc3986.uris.LazyUri;
@@ -53,7 +52,7 @@ public final class BasicOAuth2Client implements OAuth2Client
 
     public BasicOAuth2Client(OAuth2AuthorizationProvider provider, OAuth2ClientCredentials credentials, URI redirectUri)
     {
-        this(provider, credentials, new LazyUri(new Precoded(new LazyCharSequence(redirectUri))));
+        this(provider, credentials, new LazyUri(new Precoded(redirectUri == null ? "" : redirectUri.toString())));
     }
 
 
